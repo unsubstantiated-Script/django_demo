f06d5b269c04ef33ce0e977cba92b4868f4e291e
unsubstantiated-Script <jusfulton@gmail.com>
2022-05-10 07:36:22 -0700

    updated with generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,18 @@ from . import views
 
 # app_name differentiates between apps that might be clustered together on this page
 app_name = 'polls'
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+# Converting to use generic views or a templating system
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,48 @@
-from django.http import HttpResponse, HttpResponseRedirect
+
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.db.models import F
+from django.views import generic
+
 from .models import Choice, Question
 
 
-def index(request):
-    # ORM stuff going on here orders by publication date entry and only lists 5 records
-    # While template can be modified by the view, it's better to set the structure in the template
-    latest_question_list = Question.objects.order_by('-pub_date')[:6]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     # ORM stuff going on here orders by publication date entry and only lists 5 records
+#     # While template can be modified by the view, it's better to set the structure in the template
+#     latest_question_list = Question.objects.order_by('-pub_date')[:6]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
